414a5a428bc5d271b39dcc6ce0d9731d50d60551
Andres270905 <castellanos27andres@gmail.com>
2024-10-06 21:02:37 -0500

    Add files via upload

---
+++ contagiados.py
@@ -0,0 +1,10 @@
+c=57
+print('Números de contagiados hoy :',c)
+d=int(input('Número de días que han pasado:'))
+if d > 0:
+    print('Total de contagiados:',c*d)
+else:
+    print('No es valido')
+
+    
+    
